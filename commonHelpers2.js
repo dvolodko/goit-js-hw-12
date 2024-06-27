b715febeaf8eb50f779dd00c67fd9d11aa31b6c4
dvolodko <41117606+dvolodko@users.noreply.github.com>
2024-06-27 09:58:07 +0000

    Deploying to gh-pages from @ dvolodko/goit-js-hw-09@2eb5d6a43bb1db7f891b9923fded078d9b2f480b 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const t={email:"",message:""},a=document.querySelector(".feedback-form");a.addEventListener("input",n);function n(e){t[e.target.name]=e.target.value}
+//# sourceMappingURL=commonHelpers2.js.map
